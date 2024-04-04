cd543c96d1c121f3578318ff6dc806903ad74a4e
Aiden Foo <aidenfoo154@gmail.com>
2024-04-04 15:31:52 -0700

    vercel will maybe work

--- README.md
+++ README.md
@@ -13,5 +13,5 @@
 ### Peer Review:
 **Name**: Michael Heckerman
 
-**Comments**: I wish I could right a better review, but I can't right now. The site is not functional at this time. I would suggest looking at other peoples code that got it to work and see if you can get yours up and running. You can do it Aiden, I have faith in you. 
+**Comments**: I wish I could write a better review, but I can't right now. The site is not functional at this time. I would suggest looking at other peoples code that got it to work and see if you can get yours up and running. You can do it Aiden, I have faith in you. 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,14 @@ export default function Home() {
   const [total, setTotal] = useState<number>(0)
   const [numOfPeople, setNumOfPeople] = useState<number>(0)
   const [custom, setCustom] = useState<number>()
+  const [tip, setTip] = useState<number>(0)
   const [tipAmount, setTipAmount] = useState<number>(0)
   const [perPseron, setPerPerson] = useState<number>(0)
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleRadioChange = (value: string) => {
+    setSelectedValue(value);
+  };
 
   useEffect(() => {
     if (numOfPeople > 0 && perPseron > 0 && custom != undefined) {
@@ -16,7 +22,7 @@ export default function Home() {
       setTotal((total + billTip) / numOfPeople)
       setTipAmount(billTip / numOfPeople);
     }
-  }, [numOfPeople, tipAmount, total])
+  }, [numOfPeople, tipAmount, total, tip])
 
   const HandleReset = () =>{
     setTotal(0)
@@ -46,22 +52,38 @@ export default function Home() {
                       fill="#9EBBBD" d="M6.016 16.328v-1.464c1.232-.08 2.22-.444 2.964-1.092.744-.648 1.116-1.508 1.116-2.58v-.144c0-.992-.348-1.772-1.044-2.34-.696-.568-1.708-.932-3.036-1.092V4.184c.56.144 1.012.4 1.356.768.344.368.516.816.516 1.344v.288h1.824v-.432c0-.448-.088-.876-.264-1.284a3.783 3.783 0 00-.744-1.116A4.251 4.251 0 007.54 2.9a5.324 5.324 0 00-1.524-.492V.872H4.288V2.36a5.532 5.532 0 00-1.416.324c-.448.168-.84.392-1.176.672-.336.28-.604.616-.804 1.008-.2.392-.3.844-.3 1.356v.144c0 .96.316 1.708.948 2.244.632.536 1.548.884 2.748 1.044v3.912c-.704-.16-1.248-.472-1.632-.936-.384-.464-.576-1.08-.576-1.848v-.288H.256v.576c0 .464.08.924.24 1.38.16.456.404.88.732 1.272.328.392.744.728 1.248 1.008s1.108.476 1.812.588v1.512h1.728zM4.288 7.424c-.688-.128-1.164-.332-1.428-.612-.264-.28-.396-.644-.396-1.092 0-.464.176-.832.528-1.104.352-.272.784-.448 1.296-.528v3.336zm1.728 5.712V9.344c.768.128 1.328.328 1.68.6.352.272.528.688.528 1.248 0 .544-.196.984-.588 1.32-.392.336-.932.544-1.62.624z" />
                   </svg>
                 </div>
-                <input type="number" id="input-group-1"  className="text-right text-[24px] bg-[#f4fafa] border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-none" placeholder="0" min={0} onChange={(e) => setTotal(e.target.valueAsNumber)}/>
+                <input
+                name="bill"
+                type="number"
+                className="w-full text-right outline-none bg-transparent no-spin"
+                value={total}
+                onChange={(e) => { 
+                  let inputValue = parseFloat(e.target.value);
+                  // Check if value is within range
+                  if (!isNaN(inputValue) && inputValue >= 0.01 && inputValue <= 1000000) {
+                    // Round to two decimal places
+                    inputValue = Math.round(inputValue * 100) / 100;
+                    setTotal(inputValue);
+                  }
+                }}
+                min={0.01}
+                max={1000000}
+              />
               </div>
             </div>
             <br />
             <div className="text-2lg tracking-widest">Select Tip %</div>
             <br />
             <div className="grid grid-cols-3 gap-3 ">
-              <button type="button" className="hover:text-[#00494d] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800">5%
+              <button type="button" className="hover:text-[#00494d] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800" onClick={() => handleRadioChange('5')}>5%
               </button>
-              <input type="radio"
-               className="hover:text-[rgb(0,73,77)] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800">10%
-              </input>
-              <input type="radio" className="hover:text-[#00494d] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:text-[#00494d] focus:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800" >15%</input>
+              <button type="button"
+               className="hover:text-[rgb(0,73,77)] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800" onClick={() => handleRadioChange('10')}>10%
+              </button>
+              <button type="button" className="hover:text-[#00494d] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:text-[#00494d] focus:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800" onClick={() => setTip(15)} >15%</button>
 
-              <input type="radio" className="hover:text-[#00494d] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800" >25%</input>
-              <input type="radio" className="hover:text-[#00494d] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800">50%</input>
+              <button type="button" className="hover:text-[#00494d] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800" onClick={() => handleRadioChange('25')} >25%</button>
+              <button type="button" className="hover:text-[#00494d] hover:bg-[#26c0ab]  active:text-[#00494d] active:bg-[#26c0ab] focus:outline-none text-[24px] text-white bg-[#00494d] font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:focus:ring-green-800" onClick={() => handleRadioChange('50')}>50%</button>
 
               <input type="number" onChange={(e) => setCustom(e.target.valueAsNumber)} className="border border-gray-300 text-gray-900 text-[24px] text-right rounded-md focus:ring-blue-500 focus:border-[#26c0ab] block  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-border-[#26c0ab] dark:focus:border-[#26c0ab] border-none bg-[#f4fafa] h-fit" placeholder="Custom" />
             </div>
