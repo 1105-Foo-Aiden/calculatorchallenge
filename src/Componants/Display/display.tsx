9d46cbfe79f1aad00ef6738a440dfc7dc8e9ba7b
Aiden Foo <aidenfoo154@gmail.com>
2025-11-11 21:13:06 -0800

    THE MATH FINALLY WORKS!!!!

--- src/Componants/Display/display.tsx
+++ src/Componants/Display/display.tsx
@@ -8,17 +8,17 @@ export default function DisplayComponent({TipPerPerson, TotalPerPerson, ResetFun
               <p className="header">Tip Amount</p>
               <p className="unit">/Person</p>
             </div>
-              <p className="display-amt">${TipPerPerson ?? 0}</p>
+              <p className="display-amt">${TipPerPerson.toFixed(2) ?? 0}</p>
         </div>
         <div className="display-row">
             <div className="display-label">
               <p className="header">Total</p>
               <p className="unit">/Person</p>
             </div>
-              <p className="display-amt">${TotalPerPerson ?? 0}</p>
+              <p className="display-amt">${!isNaN(TotalPerPerson.toFixed(2)) ? TotalPerPerson.toFixed(2) : 0}</p>
         </div>
       </div>
-      <button className="button" onClick={() => ResetFunction} disabled = {TotalPerPerson && TipPerPerson ? false : true}>Reset</button>
+      <button className="button" onClick={ResetFunction} disabled = {TotalPerPerson && TipPerPerson ? false : true}>Reset</button>
     </div>
   );
 }
